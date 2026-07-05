bcd11c79a9e80f82eef60696517a2562f4c0caba
ronaldarroyowatson <57762270+ronaldarroyowatson@users.noreply.github.com>
2026-07-05 13:23:02 -0500

    Rewire imports across Otto repos to use standalone command service layer

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from "./update/manifestResolver.js";
 export * from "./update/apiClient.js";
 export * from "./update/updateEngine.js";
 export * from "./update/versionComparator.js";
+export * from "./generated_cli/index.js";
+export * from "./generated_api/index.js";
